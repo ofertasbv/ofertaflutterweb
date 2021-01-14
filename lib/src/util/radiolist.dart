1ab4b2bd0db9d9bd24e1980e2f2c20aeb5d40978
frc <ofertasbv@gmail.com>
2021-01-14 09:03:17 -0300

    criando enum

--- lib/src/util/radiolist.dart
+++ lib/src/util/radiolist.dart
@@ -27,24 +27,104 @@ class _TesteRadioListState extends State<TesteRadioList> {
       appBar: AppBar(
         title: Text("RadioListTitle"),
       ),
-      body: ListView(
-        children: users.map((u) {
-          return RadioListTile<User>(
-            value: u,
-            groupValue: selectedUser,
-            title: Text(u.firstName),
-            subtitle: Text(u.lastName),
-            selected: selectedUser == u,
-            activeColor: Colors.green,
-            autofocus: true,
-            toggleable: true,
-            secondary: Icon(Icons.select_all_outlined),
-            onChanged: (valor) {
-              setSelectedUser(valor);
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            child: Text("dialog"),
+            onPressed: () {
+              teste();
             },
-          );
-        }).toList(),
+          ),
+        ),
       ),
+    );
+  }
+
+  teste() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return AlertDialog(
+              title: Text("Teste dialog"),
+              content: Container(
+                width: 300.0,
+                child: ListView(
+                  children: users.map((u) {
+                    return RadioListTile<User>(
+                      value: u,
+                      groupValue: selectedUser,
+                      title: Text(u.firstName),
+                      subtitle: Text(u.lastName),
+                      selected: selectedUser == u,
+                      activeColor: Colors.green,
+                      autofocus: true,
+                      toggleable: true,
+                      secondary: Icon(Icons.select_all_outlined),
+                      onChanged: (valor) {
+                        setState(() {
+                          setSelectedUser(valor);
+                        });
+                      },
+                    );
+                  }).toList(),
+                ),
+              ),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text("Cancelar"),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+
+  dialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(32.0))),
+          contentPadding: EdgeInsets.only(top: 10.0),
+          content: Container(
+            width: 300.0,
+            child: ListView(
+              children: users.map((u) {
+                return RadioListTile<User>(
+                  value: u,
+                  groupValue: selectedUser,
+                  title: Text(u.firstName),
+                  subtitle: Text(u.lastName),
+                  selected: selectedUser == u,
+                  activeColor: Colors.green,
+                  autofocus: true,
+                  toggleable: true,
+                  secondary: Icon(Icons.select_all_outlined),
+                  onChanged: (valor) {
+                    setState(() {
+                      setSelectedUser(valor);
+                    });
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+          actions: [
+            FlatButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("ok"),
+            )
+          ],
+        );
+      },
     );
   }
 }
@@ -63,6 +143,11 @@ class User {
       User(userId: 3, firstName: "Carrie", lastName: "Brown"),
       User(userId: 4, firstName: "Deep", lastName: "Sen"),
       User(userId: 5, firstName: "Emily", lastName: "Jane"),
+      User(userId: 6, firstName: "Aaron", lastName: "Jackson"),
+      User(userId: 7, firstName: "Ben", lastName: "John"),
+      User(userId: 8, firstName: "Carrie", lastName: "Brown"),
+      User(userId: 9, firstName: "Deep", lastName: "Sen"),
+      User(userId: 10, firstName: "Emily", lastName: "Jane"),
     ];
   }
 }
